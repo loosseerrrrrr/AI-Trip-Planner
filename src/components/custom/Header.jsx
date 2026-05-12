c887d7d1e0b2141ff3aa14693d89c89c2bf203e9
Akshay kumar <akshaygpay9199@gmail.com>
2026-05-12 08:43:47 +0530

    feat: Google OAuth login working

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../ui/button'
+import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
 
 function Header() {
+  const user = JSON.parse(localStorage.getItem('user'))
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenInfo) => {
+      const userInfo = await axios.get(
+        'https://www.googleapis.com/oauth2/v1/userinfo',
+        { headers: { Authorization: `Bearer ${tokenInfo.access_token}` } }
+      )
+      localStorage.setItem('user', JSON.stringify(userInfo.data))
+      window.location.reload()
+    },
+    onError: (error) => console.log(error)
+  })
+
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-      <img src='/logo.svg'/>
+    <div className='p-2 shadow-sm flex justify-between items-center'>
+      <img src='/logo.svg' />
       <div>
-        <Button>Sign In</Button>
+        {user ? (
+          <img src={user.picture} className='rounded-full w-9 h-9' />
+        ) : (
+          <Button onClick={() => login()}>Sign In</Button>
+        )}
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CreateTrip from './create-trip/index.jsx'
+import { Toaster } from './components/ui/sonner'
 import Header from './components/custom/header.jsx'
-import { Toaster } from './components/ui/sonner.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const router = createBrowserRouter([
   {
@@ -20,8 +21,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header/>
-    <Toaster />
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <Header/>
+      <Toaster />
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
