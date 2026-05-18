dc6632dd4da8736ad00ff44c981aae11f8947a33
Akshay kumar <161154336+loosseerrrrrr@users.noreply.github.com>
2026-05-18 06:59:24 +0530

    Fix: Correct spelling errors in traveler and budget descriptions

--- src/constant/options.jsx
+++ src/constant/options.jsx
@@ -2,28 +2,28 @@ export const SelectTravelesList = [
     {
         id: 1,
         title: 'Just Me',
-        desc: 'A sole traveles in exploration',
+        desc: 'A sole traveler in exploration',
         icon: '✈',
         people: '1'
     },
     {
         id: 2,
         title: 'A Couple',
-        desc: 'Two traveles in tandem',
+        desc: 'Two travelers in tandem',
         icon: '🧑‍🤝‍🧑',
         people: '2 People'
     },
     {
         id: 3,
         title: 'Family',
-        desc: 'A group of fun loving adv',
+        desc: 'A group of fun loving adventurers',
         icon: '🏠',
         people: '3 to 5 People'
     },
     {
         id: 4,
         title: 'Friends',
-        desc: 'A bunch of thrill-seekes',
+        desc: 'A bunch of thrill-seekers',
         icon: '⛵',
         people: '5 to 10 People'
     },
@@ -45,7 +45,7 @@ export const SelectBudgetOptions = [
     {
         id: 3,
         title: 'Luxury',
-        desc: 'Dont worry about cost',
+        desc: 'Don\'t worry about cost',
         icon: '💸',
     },
 ]
